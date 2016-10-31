fc523f8e0dbb9aa686dc64ab02fe811737c5acf4
walkor <walkor@workerman.net>
2016-10-31 23:29:22 +0800

    fix path

--- start.php
+++ start.php
@@ -24,7 +24,7 @@ define('GLOBAL_START', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 // 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+foreach(glob(__DIR__.'/start_*.php') as $start_file)
 {
     require_once $start_file;
 }
